6b443d3c52dc96c94f254f3433db234f01c5adc1
Jeremy Fowler <jembyfowler@gmail.com>
2024-06-25 14:47:01 -0600

    one more note

--- app/env.js
+++ app/env.js
@@ -1,6 +1,8 @@
 // Environment Variables
 
 export const dev = window.location.origin.includes('localhost')
+
+// NOTE this variable will be used in the 'api' instance of axios in the AxiosService.js
 export const baseURL = dev ? 'https://sandbox.codeworksacademy.com' : ''
 
 // NOTE these allow to be authenticated through the sandbox API to do additional actions with the API
