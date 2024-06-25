d68d15eadd80968d4cf025297ffc74de8e4cb0f7
Jeremy Fowler <jembyfowler@gmail.com>
2024-06-25 14:47:35 -0600

    more notes

--- app/env.js
+++ app/env.js
@@ -6,6 +6,7 @@ export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'https://sandbox.codeworksacademy.com' : ''
 
 // NOTE these allow to be authenticated through the sandbox API to do additional actions with the API
+// 🐕🪪
 export const domain = 'codeworksclassroom.auth0.com'
 export const audience = 'https://codeworksclassroom.com'
 export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
